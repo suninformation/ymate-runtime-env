4d5df110f6ed217a243d2d5d255cd5ef792d8d4e
suninformation <suninformation@163.com>
2017-12-05 14:31:31 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ tar zxf jdk-8u151-linux-x64.tar.gz
 
 tar zxf apache-maven-3.1.1-bin.tar.gz
 
-tar zxf apache-tomcat-7.0.54.tar.gz
+tar zxf apache-tomcat-7.0.82.tar.gz
 
 cat mysql.*|tar -zx
 tar xf MySQL-5.6.27-1.linux_glibc2.5.x86_64.rpm-bundle.tar
