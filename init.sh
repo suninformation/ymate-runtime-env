a0a738f8cb3d4af8c58e8261361a0006659ab8f4
suninformation <suninformation@163.com>
2018-05-21 03:27:10 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
 #/bin/sh
 
-echo "正在解压缩JDK..."
+echo "Unzip JDK..."
 cat jdk.*|tar -zx
 tar zxf jdk-8u151-linux-x64.tar.gz
 
-echo "正在解压缩Maven..."
+echo "Unzip Maven..."
 tar zxf apache-maven-3.1.1-bin.tar.gz
 
-echo "正在解压缩Tomcat..."
+echo "Unzip Tomcat..."
 tar zxf apache-tomcat-7.0.82.tar.gz
 
-echo "正在解压缩MySQL..."
+echo "Unzip MySQL..."
 cat mysql.*|tar -zx
 tar xf MySQL-5.6.27-1.linux_glibc2.5.x86_64.rpm-bundle.tar
 
